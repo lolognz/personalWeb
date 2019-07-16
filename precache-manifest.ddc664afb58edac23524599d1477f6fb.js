788e40068746ed62246c530749099f9ee166f39e
Lolo <lolognz@MacBook-Pro-de-Lolo.local>
2019-07-16 21:10:41 +0200

    Updates

---
+++ precache-manifest.ddc664afb58edac23524599d1477f6fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51eb8f6f39446103c1ce2b43b072f984",
+    "url": "/personalweb/index.html"
+  },
+  {
+    "revision": "da4f29271a94d67219db",
+    "url": "/personalweb/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/personalweb/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "da4f29271a94d67219db",
+    "url": "/personalweb/static/js/main.b170bc8c.chunk.js"
+  },
+  {
+    "revision": "b6068299a478d3c3d86d",
+    "url": "/personalweb/static/js/runtime~main.5a549bf3.js"
+  }
+]);
